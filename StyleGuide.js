1a1c9f5784e2c75cdfdf09d29b89c2d9a392d3b8
John Nolan <john.nolan@myunidays.com>
2017-10-30 10:40:24 +0000

    Add Javascript Hooks example to StyleGuide.js

---
+++ StyleGuide.js
@@ -0,0 +1,4 @@
+// Javascript Hooks
+//  When hooking onto DOM elements, decorate your HTML with `js-{name}`. This means you are separating your CSS from
+//  your JavaScript logic.
+var jsModules = document.getElementsByClassName('js-name');
